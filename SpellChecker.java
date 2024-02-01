37f3b790cffb9d06b3952bd4a25184afa06adff3
Lior Mekonen <lior.mekonen01@post.runi.ac.il>
2024-02-01 17:57:00 +0200

    made changes on lev

--- SpellChecker.java
+++ SpellChecker.java
@@ -22,6 +22,8 @@ public class SpellChecker {
 	}
 
 	public static int levenshtein(String word1, String word2) {
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
 		int a = word1.length();
 		int b = word2.length();
 		if (b == 0) {
